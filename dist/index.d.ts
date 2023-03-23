527739930cb51a3371d29ebea7362eced3e27a68
LiosK <contact@mail.liosk.net>
2023-03-24 08:58:52 +0900

    build: prepare for release

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -7,19 +7,19 @@
  */
 /** Represents a UUID as a 16-byte byte array. */
 export declare class UUID {
-    readonly bytes: Uint8Array;
-    /** @param bytes - 16-byte byte array */
-    constructor(bytes: Uint8Array);
+    readonly bytes: Readonly<Uint8Array>;
+    /** @param bytes - The 16-byte byte array representation. */
+    constructor(bytes: Readonly<Uint8Array>);
     /**
      * Builds a byte array from UUIDv7 field values.
      *
-     * @param unixTsMs - 48-bit `unix_ts_ms` field.
-     * @param randA - 12-bit `rand_a` field.
-     * @param randBHi - Higher 30 bits of 62-bit `rand_b` field.
-     * @param randBLo - Lower 32 bits of 62-bit `rand_b` field.
+     * @param unixTsMs - A 48-bit `unix_ts_ms` field value.
+     * @param randA - A 12-bit `rand_a` field value.
+     * @param randBHi - The higher 30 bits of 62-bit `rand_b` field value.
+     * @param randBLo - The lower 32 bits of 62-bit `rand_b` field value.
      */
     static fromFieldsV7(unixTsMs: number, randA: number, randBHi: number, randBLo: number): UUID;
-    /** @returns 8-4-4-4-12 canonical hexadecimal string representation. */
+    /** @returns The 8-4-4-4-12 canonical hexadecimal string representation. */
     toString(): string;
     /** Creates an object from `this`. */
     clone(): UUID;
@@ -34,7 +34,7 @@ export declare class UUID {
 /**
  * Generates a UUIDv7 string.
  *
- * @returns 8-4-4-4-12 canonical hexadecimal string representation
+ * @returns The 8-4-4-4-12 canonical hexadecimal string representation
  * ("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx").
  */
 export declare const uuidv7: () => string;
@@ -43,7 +43,7 @@ export declare const uuidv7obj: () => UUID;
 /**
  * Generates a UUIDv4 string.
  *
- * @returns 8-4-4-4-12 canonical hexadecimal string representation
+ * @returns The 8-4-4-4-12 canonical hexadecimal string representation
  * ("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx").
  */
 export declare const uuidv4: () => string;
